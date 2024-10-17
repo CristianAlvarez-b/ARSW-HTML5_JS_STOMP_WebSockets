12375657b52bc0b1716851e249b94664875c6e88
cristian <cristian.alvarez-b@mail.escuelaing.edu.co>
2024-10-17 07:52:28 -0500

    PARTE 1
    Co-authored-by: juli <bricenojuliana@gmail.com>

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -36,14 +36,15 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                var theObject = JSON.parse(eventbody.body)
+                console.log(eventbody);
+                alert("Point: "+theObject.x + ", " +theObject.y);
             });
         });
 
     };
-    
+
     
 
     return {
@@ -61,6 +62,7 @@ var app = (function () {
             addPointToCanvas(pt);
 
             //publicar el evento
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
         },
 
         disconnect: function () {
